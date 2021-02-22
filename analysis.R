078f324e2dc1c0a109b55a199beb2b3a41b0e345
chnguyen22 <nchristine101@gmail.com>
2021-02-22 11:54:30 -0800

    Define Ratios

--- analysis.R
+++ analysis.R
@@ -8,17 +8,49 @@ incarceration_trends <- incarceration_trends %>%
 
 recent_aapi_ratio <- incarceration_trends %>%
   filter(year == max(year)) %>%
-  filter(county_name == "King County")
+  filter(state == "WA") %>%
+  filter(county_name == "King County") %>%
   pull(aapi_ratio)
 
-  
+incarceration_trends <- incarceration_trends %>%
+  mutate(white_ratio = white_jail_pop/white_pop_15to64, na.rm = T)
 
+recent_white_ratio <- incarceration_trends %>%
+  filter(year == max(year)) %>%
+  filter(state == "WA") %>%
+  filter(county_name == "King County") %>%
+  pull(white_ratio)
   
+incarceration_trends <- incarceration_trends %>%
+  mutate(black_ratio = black_jail_pop/black_pop_15to64, na.rm = T)
+
+recent_black_ratio <- incarceration_trends %>%
+  filter(year == max(year)) %>%
+  filter(state == "WA") %>%
+  filter(county_name == "King County") %>%
+  pull(black_ratio)  
   
-  
-  
-  
-  
+incarceration_trends <- incarceration_trends %>%
+  mutate(latinx_ratio = latinx_jail_pop/latinx_pop_15to64, na.rm = T)
+
+recent_latinx_ratio <- incarceration_trends %>%
+  filter(year == max(year)) %>%
+  filter(state == "WA") %>%
+  filter(county_name == "King County") %>%
+  pull(latinx_ratio)  
+
+incarceration_trends <- incarceration_trends %>%
+  mutate(native_ratio = native_jail_pop/native_pop_15to64, na.rm = T)
+
+recent_native_ratio <- incarceration_trends %>%
+  filter(year == max(year)) %>%
+  filter(state == "WA") %>%
+  filter(county_name == "King County") %>%
+  pull(native_ratio)  
+
+
+
+
   highest_divsion <- incarceration_trends %>%
   filter(division == max(division, na.rm = T)) %>%
   pull(division)
