680867b618159d76b885007980595e32ecd49857
chnguyen22 <nchristine101@gmail.com>
2021-02-22 13:26:39 -0800

    Add Trends Charts

--- trends_graph.R
+++ trends_graph.R
@@ -1,56 +1,56 @@
 # Ratio of incarcerated racial population over its entire racial population in King County. 
 # Plot shows trends over time.
 
-install.packages("ggplot2")
 library("ggplot2")    
-incarceration_trends <- read.csv("incarceration_trends.csv")
+library(tidyverse)
+library(dplyr)
 
-incarceration_trends <- incarceration_trends %>%
+incarceration_trends_filtered <- read.csv("incarceration_filtered_for_trends.csv")
+
+incarceration_trends_filtered <- incarceration_trends_filtered %>%
   mutate(aapi_ratio = aapi_jail_pop/aapi_pop_15to64, na.rm = T)
 
-recent_aapi_ratio <- incarceration_trends %>%
-  filter(year == max(year)) %>%
+recent_aapi_ratio <- incarceration_trends_filtered %>%
   filter(state == "WA") %>%
   filter(county_name == "King County") %>%
   pull(aapi_ratio)
 
-incarceration_trends <- incarceration_trends %>%
+incarceration_trends_filtered <- incarceration_trends_filtered %>%
   mutate(white_ratio = white_jail_pop/white_pop_15to64, na.rm = T)
 
-recent_white_ratio <- incarceration_trends %>%
-  filter(year == max(year)) %>%
+recent_white_ratio <- incarceration_trends_filtered %>%
   filter(state == "WA") %>%
   filter(county_name == "King County") %>%
   pull(white_ratio)
 
-incarceration_trends <- incarceration_trends %>%
+incarceration_trends_filtered <- incarceration_trends_filtered %>%
   mutate(black_ratio = black_jail_pop/black_pop_15to64, na.rm = T)
 
-recent_black_ratio <- incarceration_trends %>%
-  filter(year == max(year)) %>%
+recent_black_ratio <- incarceration_trends_filtered %>%
   filter(state == "WA") %>%
   filter(county_name == "King County") %>%
   pull(black_ratio)  
 
-incarceration_trends <- incarceration_trends %>%
+incarceration_trends_filtered <- incarceration_trends_filtered %>%
   mutate(latinx_ratio = latinx_jail_pop/latinx_pop_15to64, na.rm = T)
 
-recent_latinx_ratio <- incarceration_trends %>%
-  filter(year == max(year)) %>%
+recent_latinx_ratio <- incarceration_trends_filtered %>%
   filter(state == "WA") %>%
   filter(county_name == "King County") %>%
   pull(latinx_ratio)  
 
-incarceration_trends <- incarceration_trends %>%
+incarceration_trends_filtered <- incarceration_trends_filtered %>%
   mutate(native_ratio = native_jail_pop/native_pop_15to64, na.rm = T)
 
-recent_native_ratio <- incarceration_trends %>%
-  filter(year == max(year)) %>%
+recent_native_ratio <- incarceration_trends_filtered %>%
   filter(state == "WA") %>%
   filter(county_name == "King County") %>%
   pull(native_ratio)  
 
 
-ggplot(data = incarceration_trends) +
-  geom_hex(mapping = aes(x = year, y = recent_aapi_ratio))
+ggplot(data = incarceration_trends_filtered) +
+  geom_point(mapping = aes(x = year, y = recent_aapi_ratio)) + ylim(0.0, 0.002) 
+
+ggplot(data = incarceration_trends_filtered) +
+  geom_point(mapping = aes(x = year, y = recent_black_ratio)) + ylim(0.0, 0.02)
 
